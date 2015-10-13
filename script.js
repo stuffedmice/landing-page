4f6a1442fd04d6c3e40c0492faf7096c28ac3a84
stuffedmice <marta@korowaj.com>
2015-10-13 21:24:47 +0100

    assign 3

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function buttonPressed(event){
+  alert("We're Not Ready For Sign-Ups... Yet.");
+}
+
+var button = document.getElementsByClassName("cta-bottom");
+button[0].addEventListener('click', buttonPressed);
